0891a5323cc8aa6d6503a4b2487e5c99af5b9b93
Wangui-quinnie <w.wangui@ymail.com>
2024-03-06 12:09:11 +0000

    fixed error handling(reject)

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then(([photoResponse, userResponse]) => {
       console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
-    })
-    .catch(() => {
-      console.error('Signup system offline');
+    }).catch(() => {
+      console.log('Signup system offline');
     });
 }
