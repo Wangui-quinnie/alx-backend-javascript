9c0e0270e5aad650f246705ee31ab080595475c4
Wangui-quinnie <w.wangui@ymail.com>
2024-03-06 13:13:56 +0000

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,12 +2,18 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
+  const sign = signUpUser(firstName, lastName);
+  const upload = uploadPhoto(fileName);
+  const status = [];
 
-  return Promise.allSettled([userPromise, photoPromise])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason.message,
-    })));
+  return Promise.allSettled([sign, upload]).then((results) => {
+    results.forEach((result) => {
+      if (result.status === 'fulfilled') {
+        status.push({ status: result.status, value: result.value });
+      } else if (result.status === 'rejected') {
+        status.push({ status: result.status, value: `${result.reason}` });
+      }
+    });
+    return status;
+  });
 }
