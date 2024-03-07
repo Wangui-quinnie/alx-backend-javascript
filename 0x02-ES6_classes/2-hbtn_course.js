52f24514e79b3ca9a4b846372cdb0befda30e176
Wangui-quinnie <w.wangui@ymail.com>
2024-03-07 13:44:40 +0000

    A Course, Getters, and Setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,48 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    // Validate input parameters
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    if (typeof length !== 'number' || length <= 0) {
+      throw new TypeError('Length must be a number');
+    }
+
+    if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+
+    // Assign validated parameters to instance properties
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Getter and setter for name
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  // Getter and setter for length
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    this._length = value;
+  }
+
+  // Getter and setter for students
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    this._students = value;
+  }
+}
